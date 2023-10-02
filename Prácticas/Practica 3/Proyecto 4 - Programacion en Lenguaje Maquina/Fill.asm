d083922c049d2155907fa834f21a87ee530632f1
joanpedraza <113645885+joanpedraza@users.noreply.github.com>
2023-10-01 23:45:44 -0500

    Cambio a Fill

--- Prácticas/Practica 3/Proyecto 4 - Programacion en Lenguaje Maquina/Fill.asm
+++ Prácticas/Practica 3/Proyecto 4 - Programacion en Lenguaje Maquina/Fill.asm
@@ -13,38 +13,38 @@
 
 // Put your code here.
 
-(RESTART)
+(Reinicio)
 @SCREEN
 D=A
 @0
 M=D	//PUT SCREEN START LOCATION IN RAM0
 
 ///////////////////////////
-(KBDCHECK)
+(PruebaTeclado)
 
 @KBD
 D=M
-@BLACK
+@PantallaNegra
 D;JGT	//JUMP IF ANY KBD KEYS ARE PRESSED
-@WHITE
+@PantallaBlanca
 D;JEQ	//ELSE JUMP TO WHITEN
 
-@KBDCHECK
+@PruebaTeclado
 0;JMP
 ///////////////////////////
-(BLACK)
+(PantallaNegra)
 @1
 M=-1	//WHAT TO FILL SCREEN WITH (-1=11111111111111)
-@CHANGE
+@Op
 0;JMP
 
-(WHITE)
+(PantallaBlanca)
 @1
 M=0	//WHAT TO FILL SCREEN WITH
-@CHANGE
+@Op
 0;JMP
 //////////////////////////
-(CHANGE)
+(Op)
 @1	//CHECK WHAT TO FILL SCREEN WITH
 D=M	//D CONTAINS BLACK OR WHITE
 
@@ -61,8 +61,8 @@ D=A-D	//KBD-SCREEN=A
 M=M+1	//INC TO NEXT PIXEL
 A=M
 
-@CHANGE
+@Op
 D;JGT	//IF A=0 EXIT AS THE WHOLE SCREEN IS BLACK
 /////////////////////////
-@RESTART
+@Reinicio
 0;JMP
